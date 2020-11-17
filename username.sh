aa03687a8acbc8ca15a792759a831e6983f48823
Jo5687 <jkaufman@chapman.edu>
2020-11-17 09:21:54 -0800

    Update regex

--- username.sh
+++ username.sh
@@ -2,11 +2,11 @@
 # username.sh
 # Jo Kaufman
 # author
-echo "Enter a username: "
+echo "Enter a username of 3-12 characters, starting lower case. You may use digits, lower case letters, and an underscore: "
 read USERNAME
-while echo "$ZIP" | egrep -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - five digits only!"
+	echo "You must enter a valid username - Start lower case, 3 - 12 lower case letters, digits, or underscore!"
 	echo "Enter a username: "
 	read USERNAME
 done
